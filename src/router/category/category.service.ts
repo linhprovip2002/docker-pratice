b93bd7bc05f74baca5d77c8f4d89a08193750c02
Minh Haong <HoangAkyc29>
2023-11-16 15:20:13 +0700

    update Category router

--- src/router/category/category.service.ts
+++ src/router/category/category.service.ts
@@ -1,12 +1,36 @@
-import { Category } from '../../database/models'
+import { Category, Product } from '../../database/models'
 
 
 class CategoryService {
     _constructor() {
     }
 
+    async updateIDProductInAllCategory() {
+        try {
+            // Xóa toàn bộ giá trị trong trường IDProduct của tất cả các Category
+            await Category.updateMany({}, { $set: { IDProduct: [] } });
+
+            // Lấy danh sách tất cả các sản phẩm
+            const allProducts = await Product.find({});
+
+            // Duyệt qua từng sản phẩm
+            for (const product of allProducts) {
+                // Thêm product._id vào IDProduct
+                await Category.updateOne(
+                    { _id: product.IDCategory },  
+                    { $addToSet: { IDProduct: product._id } }
+                );
+            }
+    
+            console.log('Update IDProduct in Category completed.');
+        } catch (error) {
+            console.error('Failed to update IDProduct in Category:', error);
+        }
+    }
+
     async getProductsByCategoryName(page, limit, slugstring) {
         try {
+            await this.updateIDProductInAllCategory();
             const category = await Category.findOne({ slug: slugstring }).populate({
                 path: 'IDProduct',
                 match: { deleted: false }
@@ -25,7 +49,8 @@ class CategoryService {
 
     async getCategorys() {
         try {
-            const categorys = await Category.find({deleted:false});
+            await this.updateIDProductInAllCategory();
+            const categorys = await Category.find({deleted:false}).populate('IDProduct');
             return categorys ;
         } catch(error) {
             throw error;
